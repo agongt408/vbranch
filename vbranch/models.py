998ad3de574b941d53ca6bf1955ca72156c2df99
agong <agongt408@gmail.com>
2019-03-22 22:27:26 -0400

    added classification ensemble

--- vbranch/models.py
+++ vbranch/models.py
@@ -6,9 +6,10 @@ import tensorflow as tf
 
 def simple_fcnet(input_tensor, *layers_spec):
     x = input_tensor
+
     for i in range(len(layers_spec)):
         x = Dense(layers_spec[i], 'fc'+str(i + 1))(x)
         x = BatchNormalization('bn'+str(i + 1))(x)
         x = Activation('relu', 'relu'+str(i + 1))(x)
-        # print(x)
+
     return x

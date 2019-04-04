7edd0b327c97bd5f94a7bbd0456d19331a693419
agongt408 <agongt408@gmail.com>
2019-04-03 20:38:05 -0400

    added train.log from run

--- run.sh
+++ run.sh
@@ -4,8 +4,15 @@
 # https://stackoverflow.com/questions/24207916/catching-the-exception-thrown-by-python-script-in-shell-script
 
 python experiment-vb.py --num_branches 4 --model_id 1 | tee train.log
-# python experiment-vb.py --num_branches 4 --model_id 2 | tee -a train.log
-# python experiment-vb.py --num_branches 4 --model_id 3 | tee -a train.log
-# python experiment-vb.py --num_branches 4 --model_id 4 | tee -a train.log
+python experiment-vb.py --num_branches 4 --model_id 2 | tee -a train.log
+python experiment-vb.py --num_branches 4 --model_id 3 | tee -a train.log
+python experiment-vb.py --num_branches 4 --model_id 4 | tee -a train.log
 
-# python experiment.py | tee train.log
+python experiment.py --model_id 1 | tee -a train.log
+python experiment.py --model_id 2 | tee -a train.log
+python experiment.py --model_id 3 | tee -a train.log
+python experiment.py --model_id 4 | tee -a train.log
+python experiment.py --model_id 5 | tee -a train.log
+python experiment.py --model_id 6 | tee -a train.log
+python experiment.py --model_id 7 | tee -a train.log
+python experiment.py --model_id 8 | tee -a train.log
